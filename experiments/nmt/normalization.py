70c1f8d23eb41f45d538b1e736da771af33a99c0
Grzegorz Chrupała <g.chrupala@uvt.nl>
2015-01-31 19:44:29 +0100

    Code for text normalization.

---
+++ experiments/nmt/normalization.py
@@ -0,0 +1,21 @@
+dict(
+source = ["/home/gchrupala/repos/GroundHog/experiments/nmt/run-0/train-0.ori.shuf.h5"],
+target = ["/home/gchrupala/repos/GroundHog/experiments/nmt/run-0/train-0.tgt.shuf.h5"],
+indx_word = "/home/gchrupala/repos/GroundHog/experiments/nmt/run-0/train-0.ori.ivocab.pkl",
+indx_word_target = "/home/gchrupala/repos/GroundHog/experiments/nmt/run-0/train-0.tgt.ivocab.pkl",
+word_indx = "/home/gchrupala/repos/GroundHog/experiments/nmt/run-0/train-0.ori.vocab.pkl",
+word_indx_trgt = "/home/gchrupala/repos/GroundHog/experiments/nmt/run-0/train-0.tgt.vocab.pkl",
+
+null_sym_source = 67,
+null_sym_target = 68,
+n_sym_source = 72,
+n_sym_target = 73,
+
+seqlen = 20,
+bs  = 16,
+
+dim = 400,
+rank_n_approx = 128,
+
+prefix = 'search_norm_'
+)
